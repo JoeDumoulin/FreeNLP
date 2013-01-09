891e5ddd968e38e751450f79fd67d1597c8a2ff2
joe dumoulin <joe.dumoulin@gmail.com>
2013-01-08 19:45:54 -0800

    missed in last commit
    added TextCorpusReader which was missed in previous commit.

--- CorpusReader/TextCorpusReader.cs
+++ CorpusReader/TextCorpusReader.cs
@@ -55,25 +55,41 @@ namespace CorpusReader
       }
     }
 
+    private IEnumerable<string> read_words(string fileid)
+    {
+      foreach (var line in read_raw(fileid))
+      {
+        foreach (var token in _splitter.Split(line))
+        {
+          if (token != String.Empty && token != " ")
+            yield return token;
+        }
+      }
+    }
+
+    private IEnumerable<string> read_all_words()
+    {
+      foreach (var f in fileids())
+      {
+        foreach (var term in read_words(f))
+        {
+          yield return term;
+        }
+      }
+
+    }
+
     public IEnumerable<string> words(string fileid = "")
     {
       var path = "";
       if (fileid != String.Empty)
-        path  = Path.Combine(_path, fileid);
+      {
+        path = Path.Combine(_path, fileid);
+        return read_words(path);
+      }
       else
       {
-        foreach (var f in fileids())
-        {
-          path  = Path.Combine(_path, f);
-          foreach (var line in read_raw(f))
-          {
-            foreach (var token in _splitter.Split(line))
-            {
-              if (token != String.Empty && token != " ")
-                yield return token;
-            }
-          }
-        }
+        return read_all_words();
       }
     }
 
